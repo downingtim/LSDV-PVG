89ba8b95e8fe7de9b32c7bf5817d1a09ced1f401
Tim Downing <downingtim@gmail.com>
2025-08-06 01:32:32 +0100

    Delete stuff

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Run in Nextflow given a template YML file and an example FASTA file. You may nee
 
 For example, we can examine a small set of goatpox virus (GTPV) genomes:
 
-    nextflow run main.nf --config template.GTPV.yml --reference test_genomes.GTPV.fa
+    nextflow run main.nf --config templates/template.GTPV.yml --reference test_data/test_genomes.GTPV.fa
 
 Note that for your own samples, you will need to remove special characters in your sample names. In addition, to ensure compatibility with other pangenome graph tools, please adhere to the [PanSN-spec: Pangenome Sequence Naming](https://github.com/pangenome/PanSN-spec) guidance, which basically means adding a hash and a digit onto the end of the sample names.
 
@@ -33,55 +33,55 @@ Note that for your own samples, you will need to remove special characters in yo
 
 In another example, we can examine a set of 142 foot-and-mouth virus (FMDV) serotype A genomes:
 
-    nextflow run main.nf --config template.FMDV.A.yml --reference test_genomes.FMDV.A.fa
+    nextflow run main.nf --config templates/template.FMDV.A.yml --reference test_data/test_genomes.FMDV.A.fa
 
 We can examine a set of 441 foot-and-mouth virus (FMDV) serotype O genomes:
 
-    nextflow run main.nf --config template.FMDV.O.yml --reference test_genomes.FMDV.O.fa
+    nextflow run main.nf --config templates/template.FMDV.O.yml --reference test_data/test_genomes.FMDV.O.fa
 
 We can examine a set of 18 foot-and-mouth virus (FMDV) serotype C genomes:
 
-    nextflow run main.nf --config template.FMDV.C.yml --reference  test_genomes.FMDV.C.fa
+    nextflow run main.nf --config templates/template.FMDV.C.yml --reference test_data/test_genomes.FMDV.C.fa
 
 We can take 121 lumpy skin disease virus (LSDV) genomes:
 
-    nextflow run main.nf --config template.LSDV.yml --reference test_genomes.LSDV.fa
+    nextflow run main.nf --config templates/template.LSDV.yml --reference test_data/test_genomes.LSDV.fa
 
 We can take 31 sheeppoxvirus (SPPV) genomes:
 
-    nextflow run main.nf --config template.SPPV.yml --reference test_genomes.SPPV.fa  
+    nextflow run main.nf --config templates/template.SPPV.yml --reference test_data/test_genomes.SPPV.fa  
 
 We can take 132 lumpy skin disease virus (LSDV) genomes but just 7.5 Kb spanning 2.5-10 Kb of their aligned genomes:
 
-    nextflow run main.nf --config template.LSDV.10kb.yml --reference test_genomes.LSDV.10kb.fa
+    nextflow run main.nf --config templates/template.LSDV.10kb.yml --reference test_data/test_genomes.LSDV.10kb.fa
 
 We can take 132 lumpy skin disease virus (LSDV) genomes but just 5 Kb spanning 135-140 Kb of their aligned genomes:
 
-    nextflow run main.nf --config template.LSDV.135kb.yml --reference test_genomes.LSDV.135kb.fa
+    nextflow run main.nf --config templates/template.LSDV.135kb.yml --reference test_data/test_genomes.LSDV.135kb.fa
 
 We can run on a large DNA test dataset - 13 GTPV genomes (at the time of writing) to be downloaded based on the text in the template file:
 
-    nextflow run main.nf --config template.GTPV.all.yml 
+    nextflow run main.nf --config templates/template.GTPV.all.yml 
 
 We can run on a ssRNA test dataset - 15 porcine respiratory coronavirus genomes to be downloaded:
 
-    nextflow run main.nf --config template.PRCV.all.yml
+    nextflow run main.nf --config templates/template.PRCV.all.yml
 
 We can investigate 2,358 mpox genomes:
 
-    nextflow run main.nf --config template.MPOX.yml --reference test_genomes.mpox.fa
+    nextflow run main.nf --config templates/template.MPOX.yml --reference test_data/test_genomes.mpox.fa
 
 We can investigate 414 Rift Valley fever virus (RVFV) S segment sequences:
 
-    nextflow run main.nf --config template.RVFV.S.yml --reference test_genomes.S.fa
+    nextflow run main.nf --config templates/template.RVFV.S.yml --reference test_data/test_genomes.S.fa
 
 We can investigate 302 Rift Valley fever virus (RVFV) M segment sequences:
 
-    nextflow run main.nf --config template.RVFV.M.yml --reference test_genomes.M.fa 
+    nextflow run main.nf --config templates/template.RVFV.M.yml --reference test_data/test_genomes.M.fa 
 
 We can investigate 306 Rift Valley fever virus (RVFV) L segment sequences:
 
-    nextflow run main.nf --config template.RVFV.L.yml  --reference test_genomes.L.fa
+    nextflow run main.nf --config templates/template.RVFV.L.yml  --reference test_data/test_genomes.L.fa
 
 
 ## Module selection
--- bin.docker/plot_pavs.R
+++ bin.docker/plot_pavs.R
@@ -30,107 +30,129 @@ if (!file.exists(input_file)) {
 cat("Reading data from:", input_file, "\n")
 start_time <- Sys.time()
 
-# Fast data reading with data.table
-x <- read.csv(input_file, sep="\t")
-colnames(x)<-c("value","count")
+# Fast data reading with proper error handling
+tryCatch({
+  x <- read.csv(input_file, sep="\t", header = FALSE)
+  colnames(x) <- c("value","count")
+  
+  # Ensure data types are correct
+  x$value <- as.numeric(x$value)
+  x$count <- as.numeric(x$count)
+  
+  # Remove any rows with NA values
+  x <- x[complete.cases(x), ]
+  
+  if (nrow(x) == 0) {
+    cat("Error: No valid data rows found in input file\n")
+    quit(status = 1)
+  }
+  
+}, error = function(e) {
+  cat("Error reading input file:", e$message, "\n")
+  quit(status = 1)
+})
+
 n_samples <- sum(x$count)
 
 cat("Data loaded successfully. Samples:", n_samples, "\n")
 cat("Data range:", min(x$value), "to", max(x$value), "\n")
+cat("Number of unique values:", nrow(x), "\n")
 
 # Calculate optimal number of bins for large datasets
 optimal_breaks <- min(62, max(30, ceiling(sqrt(n_samples))))
 cat("Using", optimal_breaks, "bins for histogram\n")
-breaks <- seq(min(x$value),max(x$value), length.out = optimal_breaks + 1)
+breaks <- seq(min(x$value), max(x$value), length.out = optimal_breaks + 1)
 
 # Create enhanced plot
 cat("Generating plot...\n")
 pdf(output_file, width = 7, height = 6)
 
-#Assign counts to intervals in x$freq
-x$interval <- findInterval(x=x$value,vec=breaks,rightmost.closed=F,left.open=F)
-x.freq <- x %>% group_by(interval) %>% summarize(sum(count))
+# Set up plotting parameters
+par(mar = c(5, 5, 4, 2) + 0.1,  # Increase margins
+    bg = "white",
+    family = "sans")
+
+# Assign counts to intervals
+x$interval <- findInterval(x = x$value, vec = breaks, rightmost.closed = FALSE, left.open = FALSE)
+x.freq <- x %>% group_by(interval) %>% summarize(total_count = sum(count), .groups = 'drop')
 colnames(x.freq) <- c("interval","count")
 
-#design a combined table
-counts.filled <- data.frame(interval=c(1:length(breaks)),count=0)
-result <- counts.filled %>% left_join(x.freq, by = "interval") %>% select(interval,count.y)
+# Design a combined table with all intervals
+counts.filled <- data.frame(interval = 1:optimal_breaks, count = 0)
+result <- counts.filled %>% 
+  left_join(x.freq, by = "interval") %>% 
+  select(interval, count.y)
 result$count.y[is.na(result$count.y)] <- 0
 
-
-#barplot
+# Create barplot
 bp <- barplot(result$count.y,
-              names.arg = round(breaks,2),
-              xlab=c("Number of samples"),
-              ylab=c("Number of mutations"),
+              names.arg = round(breaks[-length(breaks)], 3),  # Remove last break for labeling
+              xlab = "PAV values",
+              ylab = "Number of mutations",
               main = paste("Distribution of PAV values (n =", format(n_samples, big.mark = ","), "PAVs)"),
               col = "#4292C6",
               border = "#08519C",
               las = 1,
               cex.lab = 1.2,
               cex.main = 1.3,
-              cex.axis = 1.1)
+              cex.axis = 0.8)  # Smaller axis text to prevent overlap
 
+# Add grid
 grid(col = "lightgray", lty = "dotted", lwd = 1)
 
 # Get the plotting region coordinates
-par(mar = c(5, 5, 4, 2) + 0.1,  # Increase margins
-    bg = "white",
-    family = "sans")
 usr <- par("usr")
 plot_xlim <- usr[1:2]
 
-# Generate density
-
-if (n_samples > 1000) {  # Only for reasonably large datasets
+# Generate density curve for large datasets
+if (n_samples > 1000) {  
   tryCatch({
-      # Sample data for density estimation if very large
-      sample_size <- min(50000, n_samples)
-      probabilities <- x$count / sum(x$count)
-      sample_data <- sample(x$value,
-                          size = sample_size,
-                          replace = TRUE,
-                          prob = probabilities)
-      dens <- density(sample_data, na.rm = TRUE)
+    # Sample data for density estimation if very large
+    sample_size <- min(50000, n_samples)
+    probabilities <- x$count / sum(x$count)
+    sample_data <- sample(x$value,
+                        size = sample_size,
+                        replace = TRUE,
+                        prob = probabilities)
+    dens <- density(sample_data, na.rm = TRUE)
 
-      # Scale y
-      scale_factor <- max(result$count.y) / max(dens$y)
-      dens$y <- dens$y * scale_factor
+    # Scale y to match histogram scale
+    scale_factor <- max(result$count.y) / max(dens$y)
+    dens$y <- dens$y * scale_factor
 
-      # Scale x to plot coordinates
-      data_range <- range(breaks)
-      dens_x_scaled <- (dens$x - data_range[1]) / diff(data_range) * diff(plot_xlim) + plot_xlim[1]
+    # Scale x to plot coordinates
+    data_range <- range(breaks)
+    dens_x_scaled <- (dens$x - data_range[1]) / diff(data_range) * diff(plot_xlim) + plot_xlim[1]
 
-      # Add density line
-      lines(dens_x_scaled, dens$y, col = "#E31A1C", lwd = 2)
+    # Add density line
+    lines(dens_x_scaled, dens$y, col = "#E31A1C", lwd = 2)
 
   }, error = function(e) {
-   print(e)
-    cat("Note: Could not add density curve\n")
+    cat("Note: Could not add density curve:", e$message, "\n")
   })
 }
 
-
-#
-## Add summary statistics as text
+# Calculate summary statistics
 mean_val <- sum(x$value * x$count) / sum(x$count)
 
-half_total <- sum(x$count)/2
-cumulative_count <- cumsum(x$count)
+# Calculate median
+x_sorted <- x[order(x$value), ]
+cumulative_count <- cumsum(x_sorted$count)
+half_total <- sum(x$count) / 2
 median_index <- which(cumulative_count >= half_total)[1]
-median_val <- x$value[median_index]
+median_val <- x_sorted$value[median_index]
 
+# Calculate standard deviation
 variance <- sum(x$count * (x$value - mean_val)^2) / sum(x$count)
 sd_val <- sqrt(variance)
 
-#
-## Position text in upper left
-legend("topleft", 
-       legend = c(paste("Mean:", round(mean_val, 2)),
-                  paste("Median:", round(median_val, 2)),
-                  paste("SD:", round(sd_val, 2))),
+# Position text in upper left
+legend("topleft",
+       legend = c(paste("Mean:", round(mean_val, 4)),
+                  paste("Median:", round(median_val, 4)),
+                  paste("SD:", round(sd_val, 4))),
        bty = "n",  # No box around legend
-       cex = 1.3,
+       cex = 1.1,
        text.col = "#333333")
 
 dev.off()
@@ -140,5 +162,4 @@ execution_time <- round(as.numeric(difftime(end_time, start_time, units = "secs"
 
 cat("Plot saved to:", output_file, "\n")
 cat("Execution time:", execution_time, "seconds\n")
-cat("Memory usage:", format(object.size(x), units = "MB"), "\n")
 
